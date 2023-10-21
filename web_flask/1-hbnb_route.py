4c8d890e3ebc6abddb6f6244dbb7148dd20706f2
robel <robelwon75@gmail.com>
2023-10-21 18:34:41 +0300

    task 1: pycodestyle fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,7 +4,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -12,10 +12,12 @@ def hello():
     ''' say hello '''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     ''' show hbnb '''
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
